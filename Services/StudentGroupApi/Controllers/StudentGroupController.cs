8b63e5718e4ef436ba928a0b5ce52c786be8a18f
Pyotr <2chilavert3@mail.ru>
2020-11-22 17:47:56 +0300

    Сохранение контроллера перед разбиением на два.

--- Services/StudentGroupApi/Controllers/StudentGroupController.cs
+++ Services/StudentGroupApi/Controllers/StudentGroupController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentGroup.Infrastracture.Shared.Managers;
 
 namespace StudentGroup.Services.Api.Controllers
 {
@@ -6,6 +7,13 @@ namespace StudentGroup.Services.Api.Controllers
     [ApiController]
     public class StudentGroupController : ControllerBase
     {
+        private readonly ISchoolManager _schoolManager;
+
+        public StudentGroupController(ISchoolManager schoolManager)
+        {
+            _schoolManager = schoolManager;
+        }
+
         [HttpGet("Index")]
         public string Index()
         {
