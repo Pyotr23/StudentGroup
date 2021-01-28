5b4d847e85e214ff88321ddc5a6c9202d59f6476
Pyotr <2chilavert3@mail.ru>
2021-01-28 23:40:13 +0300

    Свойство Id обязательное.

--- School.Api/Resources/StudentResources/FullStudentResource.cs
+++ School.Api/Resources/StudentResources/FullStudentResource.cs
@@ -1,8 +1,11 @@
-﻿namespace School.Api.Resources.StudentResources
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School.Api.Resources.StudentResources
 {
     public record FullStudentResource : SaveStudentResource
     {
-        public int Id { get; init; }        
+        [Required]
+        public int Id { get; init; }
         public string GroupNamesToString { get; init; }
     }
 }
--- School.Api/Resources/StudentResources/StudentResource.cs
+++ School.Api/Resources/StudentResources/StudentResource.cs
@@ -1,7 +1,10 @@
-﻿namespace School.Api.Resources.StudentResources
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace School.Api.Resources.StudentResources
 {
     public record StudentResource : SaveStudentResource
     {
+        [Required]
         public int Id { get; init; }       
     }
 }
